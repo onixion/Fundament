ad03ab315c16311951c8b9bb4134e03b716517a1
Alin Andersen <alin@alin.space>
2020-08-29 20:39:28 +0200

    Updated

---
+++ GroundWork.Tests/ArgumentTests.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace GroundWork.Tests
+{
+    /// <summary>
+    /// Unit tests for <see cref="Argument"/>.
+    /// </summary>
+    [TestClass]
+    public class ArgumentTests
+    {
+        /// <summary>
+        /// Test NotNull method.
+        /// </summary>
+        [TestMethod]
+        public void NotNull()
+        {
+            Argument.NotNull("TestName", new object());
+        }
+
+        /// <summary>
+        /// Test NotNull method.
+        /// </summary>
+        [TestMethod]
+        public void NotNull2()
+        {
+            Assert.ThrowsException<ArgumentNullException>(() => Argument.NotNull("TestName", null));
+        }
+
+        /// <summary>
+        /// Test Evaluate method.
+        /// </summary>
+        [TestMethod]
+        public void Evaluate()
+        {
+            Argument.Evaluate("TestName", "Test", str => str == "Test");
+        }
+
+        /// <summary>
+        /// Test Evaluate method.
+        /// </summary>
+        [TestMethod]
+        public void Evaluate2()
+        {
+            Assert.ThrowsException<ArgumentException>(() => Argument.Evaluate("TestName", "Test", _ => false));
+        }
+    }
+}
---
+++ GroundWork.Tests/TryTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroundWork.Tests
+{
+    [TestClass]
+    public class TryTests
+    {
+        [TestMethod]
+        public void CatchIgnore()
+        {
+            Try.CatchIgnore(() => { });
+        }
+
+        [TestMethod]
+        public void CatchIgnore2()
+        {
+            Try.CatchIgnore(() => throw new InvalidCastException());
+        }
+
+        [TestMethod]
+        public async Task CatchIgnoreAsync()
+        {
+            await Try.CatchIgnoreAsync(() => Task.CompletedTask);
+        }
+
+        [TestMethod]
+        public async Task CatchIgnoreAsync2()
+        {
+            await Try.CatchIgnoreAsync(() => throw new InvalidCastException());
+        }
+    }
+}
